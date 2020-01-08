b9ad0a47344a164da396b555ee38a181f5087f9b
Fay Wang <v-faywan@microsoft.com>
2020-01-08 14:32:14 +0800

    Add project files.

---
+++ Dll2_char/test.cpp
@@ -0,0 +1,12 @@
+// MathLibrary.cpp : Defines the exported functions for the DLL.
+#include "pch.h" // use stdafx.h in Visual Studio 2017 and earlier
+#include <utility>
+#include <limits.h>
+#include "test.h"
+
+
+int test(LPCTSTR name)
+{
+    return 100;
+}
+
---
+++ Dll2_char/test.h
@@ -0,0 +1,13 @@
+// MathLibrary.h - Contains declarations of math functions
+#pragma once
+
+#ifdef MATHLIBRARY_EXPORTS
+#define MATHLIBRARY_API __declspec(dllexport)
+#else
+#define MATHLIBRARY_API __declspec(dllimport)
+#endif
+
+
+extern "C" MATHLIBRARY_API int test(LPCTSTR name);
+
+
